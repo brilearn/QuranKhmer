4be131d9d98f56c7e734f00906372dd45fb5bd54
Omar <wolvthemes@gmail.com>
2025-01-15 18:55:22 +0200

    Fixing

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -8,7 +8,7 @@ import styles from './Navbar.module.scss';
 import NavbarBody from './NavbarBody';
 
 import { useOnboarding } from '@/components/Onboarding/OnboardingProvider';
-import Button, { ButtonSize, ButtonType } from '@/dls/Button/Button';
+// import Button, { ButtonSize, ButtonType } from '@/dls/Button/Button';
 import { selectNavbar } from '@/redux/slices/navbar';
 import { getProductUpdatesUrl } from '@/utils/navigation';
 
