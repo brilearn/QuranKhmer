06a1684183444147578df5c141fc8b723bc07120
Omar <wolvthemes@gmail.com>
2025-01-17 15:23:45 +0200

    Stable

--- src/redux/defaultSettings/locales/km.ts
+++ src/redux/defaultSettings/locales/km.ts
@@ -1,8 +1,8 @@
 import DEFAULT_SETTINGS, { DefaultSettings } from '../defaultSettings';
 
-const DEFAULT_TRANSLATION = 84; // Ma Jain
+const DEFAULT_TRANSLATION = 128; // Ma Jain
 
 export default {
   ...DEFAULT_SETTINGS,
-  translations: { ...DEFAULT_SETTINGS.translations, selectedTranslations: [128] },
+  translations: { ...DEFAULT_SETTINGS.translations, selectedTranslations: [DEFAULT_TRANSLATION] },
 } as DefaultSettings;
